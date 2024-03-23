81d6c212e61ef6bf187b35780b56e18aba0178e8
calvert1212 <calvert1212@gmail.com>
2024-03-23 04:43:45 +0000

    Trying using a function

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 // Prints message based on number of args
+function printArgs(args){
 const alen = arguments.length;
+
 if (alen < 1){
-console.log('No argument');
+    console.log("No arguments");
 } else if (alen < 2) {
-    console.log('Argument found');
-} else if (alen <= 2) {
-    console.log('Arguments found');
+    console.log("Argument");
+} else {
+    console.log("Arguments");
+}
 }
