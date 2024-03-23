1a2539b0cb88bcc7907ffcc6395b5d360cea9d58
calvert1212 <calvert1212@gmail.com>
2024-03-23 05:49:59 +0000

    First Attempt

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 // Prints 3 lines, C is fun, Python is cool, JavaScript is amazing
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+const holb = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (const piece of holb) {
+  console.log(holb[] + '\n');
+}
