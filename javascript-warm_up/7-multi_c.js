2ed7acac9a576dab4acaa458db48fd7e9160d9e3
calvert1212 <calvert1212@gmail.com>
2024-03-23 06:27:07 +0000

    NOW it should work

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
 // Prints 3 lines, C is fun, Python is cool, JavaScript is amazing
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+const xnum = process.argv[2];
+for (let i = xnum; i > 0; i--) {
+  if (isNaN(xnum)) {
+    console.log('Missing number of occurences');
+    } else {
+      console.log('C is fun');
+    }
+}
