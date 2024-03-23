fbf033c5f9b084889cb4264359151cfcbd4d82ee
calvert1212 <calvert1212@gmail.com>
2024-03-23 08:20:25 +0000

    First try

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 // Prints 3 lines, C is fun, Python is cool, JavaScript is amazing
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+exports.add = function (a, b) {
+  return a + b;
+};
