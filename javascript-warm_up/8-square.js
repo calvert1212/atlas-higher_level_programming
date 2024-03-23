658428adbaea466657d22ce82d67fec9b2f671b7
calvert1212 <calvert1212@gmail.com>
2024-03-23 06:42:43 +0000

    Added string repeat and newline

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,5 +2,5 @@
 // Prints square of argv2 by argv2 X's
 const square = parseInt(process.argv[2]);
 for (let i = square; i > 0; i--) {
-  console.log('X' * square);
+  console.log('X'.repeat(square));
 }
