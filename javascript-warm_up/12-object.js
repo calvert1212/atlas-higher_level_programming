0c43706cc3cef357150d56f74d23eeb218362506
calvert1212 <calvert1212@gmail.com>
2024-03-23 08:15:18 +0000

    First try

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-// Prints 3 lines, C is fun, Python is cool, JavaScript is amazing
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+  myObject.value = 89;
+  console.log(myObject);
