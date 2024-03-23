123708250d249db7622c3a874a028463935ee0c6
calvert1212 <calvert1212@gmail.com>
2024-03-23 08:05:55 +0000

    First try

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,14 @@
 #!/usr/bin/node
 // Prints 3 lines, C is fun, Python is cool, JavaScript is amazing
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+function secBig (a, b) {
+  return a - b;
+}
+
+const arr = process.argv.slice(2);
+let max = 0;
+if (process.argv.length > 3) {
+    a.sort(sortNumber);
+    max = Number(a[a.length - 2]);
+  }
+  console.log(maxNumber);
+  
