cc85234f9c83e31e529251fa49f64e4e2ce32035
calvert1212 <calvert1212@gmail.com>
2024-03-23 05:43:10 +0000

    Fixed semicolon

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 // Prints a number followed by first arg if can be an int
 const xnum = parseInt(process.argv[2]);
 if (isNaN(xnum)) {
-  console.log('Not a number')
+  console.log('Not a number');
 } else {
   console.log('My number: ' + (xnum));
 }
