33b3b6e2aded758dc361f43fc79263f0d4494d66
calvert1212 <calvert1212@gmail.com>
2024-03-23 04:14:04 +0000

    fixed arguments.length name

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Prints message based on number of args
-if (args.length === 0){
+if (arguments.length === 0){
 console.log('No argument');
-} else if (args.length === 1){
+} else if (arguments.length === 1){
     console.log('Argument found');
 } else {
     console.log('Arguments found');
