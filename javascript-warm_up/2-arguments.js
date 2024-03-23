c3aa3c135ab9058ae5fdc12702b0abd95801acc8
calvert1212 <calvert1212@gmail.com>
2024-03-23 04:49:07 +0000

    Functionizing script to use arguments.length

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 // Prints message based on number of args
-function printArgs(args){
+function printArgs(args) {
 const alen = arguments.length;
 
-if (alen < 1){
+if (alen < 1) {
     console.log("No arguments");
 } else if (alen < 2) {
-    console.log("Argument");
+    console.log("Argument found");
 } else {
-    console.log("Arguments");
+    console.log("Arguments found");
 }
 }
+
+// Calls function
+printArgs();
