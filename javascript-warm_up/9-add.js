15ba79489aacafe9f03a4a788f02026e56129011
calvert1212 <calvert1212@gmail.com>
2024-03-23 07:22:22 +0000

    Trying this

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 // Prints 3 lines, C is fun, Python is cool, JavaScript is amazing
-const a = parseInt(process.argv[2]),b = parseInt(process.argv[3]);
+const a = process.argv[2],b = process.argv[3];
 function add(a, b) {
-  let c = a + b;
+  if (isNaN(a) || isNaN(b)) {
+    console.log('NaN')
+  } else {
+  const c = Number(a) + Number(b);
   console.log(parseInt(c));
+  }
 }
-
 add();
