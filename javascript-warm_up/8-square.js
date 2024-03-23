7c110641132519d23ccf275a1bbd2acaf7ae4443
calvert1212 <calvert1212@gmail.com>
2024-03-23 06:47:27 +0000

    Fixing validation

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 // Prints square of argv2 by argv2 X's
 const square = parseInt(process.argv[2]);
 if (isNaN(square)) {
-  console.log('Missing size')
+  console.log('Missing size');
 } else {
-for (let i = square; i > 0; i--) {
-  console.log('X'.repeat(square));
-}
+  for (let i = square; i > 0; i--) {
+    console.log('X'.repeat(square));
+  }
 }
