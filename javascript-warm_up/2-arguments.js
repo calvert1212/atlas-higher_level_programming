1508d3853506c520367f72a73deb63612d292153
calvert1212 <calvert1212@gmail.com>
2024-03-23 04:27:46 +0000

    Fixed syntax

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Prints message based on number of args
-let alen = arguments.length;
+const alen = arguments.length;
 if (alen < 1){
 console.log('No argument');
-} else if (alen < 2){
+} else if (alen < 2) {
     console.log('Argument found');
 } else if (alen <= 2) {
     console.log('Arguments found');
