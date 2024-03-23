4dec6a1b2db3444fcf1e2d744b2e340b33ca6d0b
calvert1212 <calvert1212@gmail.com>
2024-03-23 06:45:58 +0000

    Added isNaN

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 // Prints square of argv2 by argv2 X's
 const square = parseInt(process.argv[2]);
+if (isNaN(square)) {
+  console.log('Missing size')
+} else {
 for (let i = square; i > 0; i--) {
   console.log('X'.repeat(square));
+}
 }
