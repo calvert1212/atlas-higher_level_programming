eb1ee503190f293939347897fb4cef9f616c638a
calvert1212 <calvert1212@gmail.com>
2024-03-23 07:42:55 +0000

    First try

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,3 +1,14 @@
 #!/usr/bin/node
 // Prints 3 lines, C is fun, Python is cool, JavaScript is amazing
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+function fact(a) {
+  let f = parseInt(a);
+  if (isNaN(f) || f <= 0) {
+    return 1;  
+    } else {
+        return (f * fact(f - 1));
+  }
+}
+  
+const a = process.argv[2];
+console.log(fact(a));
+  
