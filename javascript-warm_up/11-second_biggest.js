f9c42674203b09db00b9fc0799a24beffec03cb6
calvert1212 <calvert1212@gmail.com>
2024-03-23 08:08:41 +0000

    Fixing

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -7,7 +7,7 @@ function secBig (a, b) {
 const a = process.argv.slice(2);
 let max = 0;
 if (process.argv.length > 3) {
-    a.sort(sortNumber);
+    a.sort(secBig);
     max = Number(a[a.length - 2]);
   }
   console.log(maxNumber);
