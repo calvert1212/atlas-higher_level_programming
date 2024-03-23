7b64682102db278ede0481b1f14147f636843ee3
calvert1212 <calvert1212@gmail.com>
2024-03-23 05:55:15 +0000

    Removing new lines?

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Prints 3 lines, C is fun, Python is cool, JavaScript is amazing
-const holb = ["C is fun\n", "Python is cool\n", "JavaScript is amazing"];
+const holb = ["C is fun", "Python is cool", "JavaScript is amazing"];
 for (const piece of holb) {
   console.log(piece);
 }
