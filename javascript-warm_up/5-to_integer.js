b7f0deb300fd88978bb312eb741f605b3896698c
calvert1212 <calvert1212@gmail.com>
2024-03-23 05:42:20 +0000

    Changed to argv2

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Prints a number followed by first arg if can be an int
-const xnum = parseInt(process.argv[3]);
+const xnum = parseInt(process.argv[2]);
 if (isNaN(xnum)) {
   console.log('Not a number')
 } else {
