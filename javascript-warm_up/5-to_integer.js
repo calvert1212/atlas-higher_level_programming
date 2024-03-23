89573dc12acac4ff6b7b52e05a6b05cdd38077db
calvert1212 <calvert1212@gmail.com>
2024-03-23 06:32:38 +0000

    Added new line at eof, fixing messed up task 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 // Prints a message a number of times based on first arg
 const xnum = process.argv[2];
-for (let i = xnum; i > 0; i--) {
-  if (isNaN(xnum)) {
-    console.log('Missing number of occurences');
-    } else {
-      console.log('C is fun');
-    }
+if (isNaN(xnum)) {
+  console.log('Not a number');
+  } else {
+    console.log('My number: ' + (xnum));
 }
