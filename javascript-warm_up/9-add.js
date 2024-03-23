fec4ce73d59523ed94612c77749dabf59a8f43d3
calvert1212 <calvert1212@gmail.com>
2024-03-23 07:27:31 +0000

    Fixing validation

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 // Prints 3 lines, C is fun, Python is cool, JavaScript is amazing
 function add (a, b) {
-    const c = Number(a) + Number(b);
-    return c;
- }
-  
+  const c = Number(a) + Number(b);
+  return c;
+}
   const a = process.argv[2];
   const b = process.argv[3];
-  
+
   if (isNaN(a) || isNaN(b)) {
-    console.log('NaN');
+console.log('NaN');
  } else {
     console.log(add(a, b));
  }
