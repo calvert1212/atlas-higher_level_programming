315d63debb303f706a3f3e2d8b051f15cb8406c3
calvert1212 <calvert1212@gmail.com>
2024-03-23 05:07:34 +0000

    de-functionized and fixing style

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,18 +1,10 @@
 #!/usr/bin/node
 // Prints message based on number of args
-
-function printArgs (args) {
-
 const aleng = process.argv.length - 2;
-
 if (aleng < 1) {
-    console.log('No argument');
-} else if (aleng === 1) {
-    console.log('Argument found');
+  console.log('No argument');
+} else if (aleng < 2) {
+  console.log('Argument found');
 } else {
     console.log('Arguments found');
 }
-}
-
-// Calls function
-printArgs();
