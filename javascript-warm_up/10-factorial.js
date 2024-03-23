e042a5bdc592c262a2006e9804c28be19715b0d3
calvert1212 <calvert1212@gmail.com>
2024-03-23 07:45:44 +0000

    Fixing validation

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 // Prints 3 lines, C is fun, Python is cool, JavaScript is amazing
-function fact(a) {
-  let f = parseInt(a);
+function fact (a) {
+  const f = parseInt(a);
   if (isNaN(f) || f <= 0) {
-    return 1;  
-    } else {
-        return (f * fact(f - 1));
+    return 1;
+  } else {
+    return (f * fact(f - 1));
   }
 }
-  
+
 const a = process.argv[2];
 console.log(fact(a));
-  
