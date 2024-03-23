7e5059853a04df01a4790a828ca7aff11ecb2cb8
calvert1212 <calvert1212@gmail.com>
2024-03-23 07:29:12 +0000

    Fixing validation

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -4,11 +4,11 @@ function add (a, b) {
   const c = Number(a) + Number(b);
   return c;
 }
-  const a = process.argv[2];
-  const b = process.argv[3];
+const a = process.argv[2];
+const b = process.argv[3];
 
-  if (isNaN(a) || isNaN(b)) {
-console.log('NaN');
- } else {
-    console.log(add(a, b));
- }
+if (isNaN(a) || isNaN(b)) {
+  console.log('NaN');
+} else {
+  console.log(add(a, b));
+}
