81185ba05d321d483177eba93f59d071ee3bb21d
calvert1212 <calvert1212@gmail.com>
2024-03-23 06:40:21 +0000

    added parseInt

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Prints square of argv2 by argv2 X's
-const square = process.argv[2];
+const square = parseInt(process.argv[2]);
 for (let i = square; i > 0; i--) {
   console.log('X' * square);
 }
