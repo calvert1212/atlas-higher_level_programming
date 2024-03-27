5a6696baa56c34dfd25af18146d843daa922a095
calvert1212 <calvert1212@gmail.com>
2024-03-27 02:20:43 +0000

    Seeing what sticks

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,8 +3,9 @@
 class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h))
-      this.width = w;
-      this.height = h;
+      return {};
+    this.width = w;
+    this.height = h;
    }
 }
 //Exports class to a module
