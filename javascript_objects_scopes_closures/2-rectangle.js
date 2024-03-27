b8fb122de95f802b715ce19641138338cca1b64b
calvert1212 <calvert1212@gmail.com>
2024-03-27 02:17:22 +0000

    Fixed indents

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,10 @@
 // Class that defines a rectangle
 class Rectangle {
   constructor(w, h) {
-  if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h))
-  this.width = w;
-  this.height = h;
- }
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h))
+      this.width = w;
+      this.height = h;
+   }
 }
 //Exports class to a module
 module.exports = Rectangle;
