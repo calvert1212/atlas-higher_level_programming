be80578d7bce233e3b1885bdd24eaee85eb5ee78
calvert1212 <calvert1212@gmail.com>
2024-03-27 03:28:12 +0000

    Checking 3

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,6 +3,6 @@
 const xnum = process.argv[2];
 if (isNaN(xnum)) {
   console.log('Not a number');
-  } else {
-    console.log('My number: ' + (xnum));
+} else {
+  console.log('My number: ' + (xnum));
 }
--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,22 +1,15 @@
 #!/usr/bin/node
-
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (w >= 1 && h >= 1) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    }
   }
-}
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      let row = '';
-    ]  for (let j = 0; j < this.width; j++) {
-         row += 'X';
-    }
-      console.log(row);
+      console.log('X'.repeat(this.width));
     }
   }
-}
-
-module.exports = Rectangle;
+};
