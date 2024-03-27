ac3d225e030c9c49af775e0ae5d7ba4624f33640
calvert1212 <calvert1212@gmail.com>
2024-03-27 01:55:28 +0000

    Is the output okay?

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Class that defines a rectangle
+class Rectangle {
+  constructor(w, h) {
+  if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+    // If w or h is 0 on not int, create empty object
+    return {};
+    }
+  this.width = w;
+  this.height = h;
+ }
+}
+//Exports class to a module
+module.exports = Rectangle;
