7a985eed787645d95d091a643e1aea06504f8dfa
calvert1212 <calvert1212@gmail.com>
2024-03-27 03:22:14 +0000

    Uploading work

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,8 +3,7 @@
 class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      let emptyRect = new Rectangle();
-      return;
+      class {};
     }
     this.width = w;
     this.height = h;
