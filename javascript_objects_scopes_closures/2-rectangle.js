6929374b3a0d8a4b89520e29a38d3a7238f3de68
calvert1212 <calvert1212@gmail.com>
2024-03-27 02:04:33 +0000

    return Rectangle();?

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,7 +4,7 @@ class Rectangle {
   constructor(w, h) {
   if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
     // If w or h is 0 on not int, create empty object
-    return Rectangle;
+    return Rectangle();
     }
   this.width = w;
   this.height = h;
