860a4f18a52972e6614e29fc42960cc57b2913ad
calvert1212 <calvert1212@gmail.com>
2024-03-30 07:29:48 +0000

    Checking 0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//Reads the contents of a file
+
+const fServer = require('fs');
+const file = process.argv[2];
+fServer.readFile(file, 'utf-8', function (error, content) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(content);
+  }
+});
---
+++ javascript-web_scraping/README.md
@@ -0,0 +1,1 @@
+Learning about webscraping with Javascript!
