34daeb835ede9a7bebeb42efbc54ad58337da6e7
calvert1212 <calvert1212@gmail.com>
2024-04-02 08:42:29 +0000

    Trying a fix

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,6 +5,9 @@ const request = require('request');
 const movieId = process.argv[2];
 const website = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 request(website, function (error, _response, body) {
+  if (!movieId){
+    process.exit(1);
+  }
   if (error) {
     console.error(error);
   } else {
