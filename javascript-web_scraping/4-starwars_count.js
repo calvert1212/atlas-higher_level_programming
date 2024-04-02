0802154bc03dc1764224f29384cb1a3db97a16b1
calvert1212 <calvert1212@gmail.com>
2024-04-02 09:38:35 +0000

    Pushing work so far

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,16 +2,20 @@
 // Prints # of movies where "Wedge" appears
 
 const request = require('request');
-const movieId = process.argv[2];
-const website = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-request(website, function (error, _response, body) {
-  if (!movieId){
+const mId = process.argv[2];
+const site = 'https://swapi-api.hbtn.io/api/films/' + mId;
+const gW = require('results.filter');
+const wF = 'https://swapi-api.hbtn.io/api/people/18/';
+
+request(site, function (error, _response, body) {
+  if (!mId) {
     process.exit(1);
   }
   if (error) {
     console.error(error);
   } else {
-    const jsonReturn = JSON.parse(body);
-    console.log(jsonReturn.title);
+    const fD = JSON.parse(body);
+    const w = fD(gW)(film => film.characters.includes(wF));
+    console.log(w);
   }
 });
