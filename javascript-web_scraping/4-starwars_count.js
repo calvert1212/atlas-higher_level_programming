30e32b7fb9695d835e39c1c5e362f93b8ec36e50
calvert1212 <calvert1212@gmail.com>
2024-04-02 09:51:52 +0000

    Fixing

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,13 +3,12 @@
 
 const request = require('request');
 const mId = process.argv[2];
-const site = 'https://swapi-api.hbtn.io/api/films/' + mId;
 const wF = 'https://swapi-api.hbtn.io/api/people/18/';
 
-request(site, function (error, response, body) {
+request(mId, function (error, response, body) {
   if (!mId || error) {
     console.error(error);
-   } else {
+  } else {
     const fD = JSON.parse(body);
     const w = fD.results.filter(film => film.characters.includes(wF));
     console.log(w);
